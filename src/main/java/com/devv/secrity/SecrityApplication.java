29db06c04e2a134ccec735ce4f134da491e4dc15
DspStudent <devisriprasad948@gmail.com>
2024-06-01 19:49:53 +0530

    first

---
+++ src/main/java/com/devv/secrity/SecrityApplication.java
@@ -0,0 +1,13 @@
+package com.devv.secrity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecrityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecrityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devv/secrity/SecrityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devv.secrity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecrityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
